187b9f5a6e46c50443d6e7f7e8004f03647c215e
Mostafa Safipour <safipour@amazon.com>
2023-01-11 13:49:13 +1100

    deploy during the build stage using sam deploy command

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world v3",
+            "message": "hello world v4",
             # "location": ip.text.replace("\n", "")
         }),
     }
